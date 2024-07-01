0d8c924ec239b290b8df284ba828871eba8d0e69
Nerdy Gedoni <gedoniani@gmail.com>
2024-07-01 17:33:49 +0100

    update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -574,14 +574,13 @@
                         <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
                             <div class="group cursor-pointer text-center">
                                 <div class="relative h-[280px] rounded-3xl transition-all duration-500 group-hover:shadow-[0_0_25px_#979797]">
-                                    <img src="/assets/images/team-member-1.png" alt="team-member-1" class="h-full w-full rounded-3xl object-cover object-top" />
+                                    <img src="/assets/images/picm.jpeg" alt="team-member-1" class="h-full w-full rounded-3xl object-cover object-top" />
                                 </div>
                                 <h4 class="pt-5 pb-2 text-xl font-extrabold text-black transition duration-500 group-hover:text-secondary dark:text-white">
-                                    Ebite Marvis
+                                    Ebite Marvis Ogochukwu
                                 </h4>
                                 <h6 class="text-sm font-bold">Software Developer</h6>
-                            </div>
-    
+                            </div>                            
                         </div>
                     </div>
                 </section>
